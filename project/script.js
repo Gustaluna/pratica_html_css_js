d1c8d6178f16d27ee0186747ec39325bb2b49263
Gustaluna <lgsantana.cin@gmail.com>
2023-01-07 19:39:37 -0300

    finishing moblie

--- project/script.js
+++ project/script.js
@@ -4,3 +4,10 @@ const menu = document.querySelector('.menu-lateral')
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
 })
+
+const botaoMenuInterno = document.querySelector('.cabecalho__menu_interno')
+const menuInterno = document.querySelector('.menu-lateral')
+
+botaoMenuInterno.addEventListener('click', () => {
+    menuInterno.classList.toggle('menu-lateral--ativo')
+})
