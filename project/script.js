c2720ea560a2714e8d364981540d3aa3acea1b82
Gustaluna <lgsantana.cin@gmail.com>
2023-01-13 23:56:22 -0300

    mobile adjust

--- project/script.js
+++ project/script.js
@@ -1,13 +1,16 @@
 const botaoMenu = document.querySelector('.cabecalho__menu')
 const menu = document.querySelector('.menu-lateral')
+const botaoMenuInterno = document.querySelector('.cabecalho__menu_interno')
+const menuInterno = document.querySelector('.menu-lateral')
+const overlay = document.querySelector('.overlay')
 
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
+    overlay.classList.toggle('overlay--ativo')
 })
-
-const botaoMenuInterno = document.querySelector('.cabecalho__menu_interno')
-const menuInterno = document.querySelector('.menu-lateral')
 
 botaoMenuInterno.addEventListener('click', () => {
     menuInterno.classList.toggle('menu-lateral--ativo')
+    overlay.classList.toggle('overlay--ativo')
 })
+
